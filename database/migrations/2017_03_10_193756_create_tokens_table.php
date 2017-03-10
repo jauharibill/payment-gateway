16d01d8957c66847b21b03723a341db22175d4d9
billxcode <iam@billtanthowijauhari.net>
2017-03-11 02:43:01 +0700

    add migration token

--- database/migrations/2017_03_10_193756_create_tokens_table.php
+++ database/migrations/2017_03_10_193756_create_tokens_table.php
@@ -15,7 +15,11 @@ class CreateTokensTable extends Migration
     {
         Schema::create('tokens', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('token')->unique();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
