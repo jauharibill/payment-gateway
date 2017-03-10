a770ca3b2a3e2a75ef7bea54f41f6321ce308226
billxcode <iam@billtanthowijauhari.net>
2017-03-11 02:38:06 +0700

    add token migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use JWTAuth;
 
 class HomeController extends Controller
 {
@@ -14,6 +15,7 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('jwt.auth');
     }
 
     /**
@@ -23,6 +25,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', compact('token'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
 
                 <div class="panel-body">
                     You are logged in!
+                    {{$token}}
                 </div>
             </div>
         </div>
